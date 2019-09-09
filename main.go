3dc3a80b4cb5bbfa48d346263f4642cffc32ce42
take <chantake33@gmail.com>
2019-09-09 11:21:42 +0900

    update

--- main.go
+++ main.go
@@ -5,14 +5,21 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"time"
 )
 
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", dumprequest)
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/dump", dumprequest)
+	mux.HandleFunc("/wait", wait)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, world\n")
 }
 
 func dumprequest(w http.ResponseWriter, r *http.Request) {
@@ -20,4 +27,9 @@ func dumprequest(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "This is echo service\n")
 	io.WriteString(w, "===DumpRequest===\n")
 	io.WriteString(w, string(dump))
+}
+
+func wait(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(10 * time.Second)
+	io.WriteString(w, "Waited 10 seconds \n")
 }
