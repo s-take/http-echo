870c5e7ab2fb275a36ea100153298483c6d0c56c
take <chantake33@gmail.com>
2019-09-19 17:49:07 +0900

    update

--- main.go
+++ main.go
@@ -20,19 +20,20 @@ func main() {
 
 func dump(w http.ResponseWriter, r *http.Request) {
 	dump, _ := httputil.DumpRequest(r, true)
-	io.WriteString(w, "This is v2 echo service\n")
+	io.WriteString(w, "This is echo service\n")
 	io.WriteString(w, "===DumpRequest===\n")
 	io.WriteString(w, string(dump))
 }
 
 func slow(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "This is v2 echo service\n")
+	io.WriteString(w, "This is echo service\n")
 	time.Sleep(10 * time.Second)
 	io.WriteString(w, "Waited 10 seconds \n")
 }
 
 func err(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusServiceUnavailable)
-	io.WriteString(w, "This is v2 echo service\n")
+
+	io.WriteString(w, "This is echo service\n")
 	io.WriteString(w, "Error!! \n")
 }
